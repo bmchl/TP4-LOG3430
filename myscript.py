1d6665ba11ad5ab057a3cdff2f743c08328e87ad
Michael Banna <michael.banna@polymtl.ca>
2025-03-23 22:34:25 -0400

    Update myscript.py

--- myscript.py
+++ myscript.py
@@ -1,4 +1,14 @@
 import os
-os.system("git bisect start $badhash $goodhash")
-os.system("git bisect run python manage.py test")
+import sys
+
+result = os.system("git bisect start $badhash $goodhash")
+if result != 0:
+    sys.exit(result)
+
+result = os.system("git bisect run python manage.py test")
+test_failed = result != 0
+
 os.system("git bisect reset")
+
+if test_failed:
+    sys.exit(1)
