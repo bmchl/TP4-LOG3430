e8a711c5e7ee9bfea4952e236c253ec5b080bfba
Michael Banna <michael.banna@polymtl.ca>
2025-03-23 22:32:27 -0400

    Update myscript.py

--- myscript.py
+++ myscript.py
@@ -1,11 +1,4 @@
 import os
-import sys
-
-bad_hash = sys.argv[1]  
-good_hash = sys.argv[2] 
-
 os.system("git bisect start $badhash $goodhash")
-
 os.system("git bisect run python manage.py test")
-
 os.system("git bisect reset")
